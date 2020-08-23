1b4df5611e1a7b79cc16568f9ab4507f991b31bf
SR SOHAG <Sohag@SRs-MacBook-Pro.local>
2020-08-24 01:20:54 +0800

    Basic Arithmatic

---
+++ number.js
@@ -0,0 +1,11 @@
+/*
+ * Basic Arithematic Calculation (LWHH JS-1.3) 
+ * To Run type : node number.js value1 value2 
+ */
+
+let value1 = parseInt(process.argv[2]);
+let value2 = parseInt(process.argv[3]);
+console.log("After Addition %d",value1+value2);
+console.log("After substraction %d",value1-value2);
+console.log("After multiplication %d",value1*value2);
+console.log("After division %d",value1/value2);
